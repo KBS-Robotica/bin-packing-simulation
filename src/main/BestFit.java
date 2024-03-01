2db9e122c1a426cc96293bd9898a777f633e3f1a
RenzeBoerman <mail@renzeboerman.nl>
2024-03-01 11:33:08 +0100

    Best fit working

--- src/main/BestFit.java
+++ src/main/BestFit.java
@@ -19,22 +19,26 @@ public class BestFit {
         boxes.add(new ArrayList<>());
 
         weights.forEach((weight) -> {
-            boolean addedToExistingBox = false;
-
-            boxes.stream().filter((box) -> Utils.calculateSum(box) + weight <= MAX_WEIGHT).;
-
-            for (ArrayList<Integer> box : boxes) {
+            Integer bestBoxIndex = null;
+            for (int i = 0; i < boxes.size(); i++) {
+                ArrayList<Integer> box = boxes.get(i);
                 int currentBoxWeight = Utils.calculateSum(box);
 
-                if (currentBoxWeight + weight <= MAX_WEIGHT) {
-//                    box.add(weight);
-//                    addedToExistingBox = true;
-//                    break;
-
+                if (bestBoxIndex == null) {
+                    if (currentBoxWeight + weight <= MAX_WEIGHT) {
+                        bestBoxIndex = i;
+                    }
+                } else if (
+                        currentBoxWeight + weight <= MAX_WEIGHT &&
+                        Utils.calculateSum(boxes.get(bestBoxIndex)) < currentBoxWeight + weight
+                ) {
+                    bestBoxIndex = i;
                 }
             }
 
-            if (!addedToExistingBox) {
+            if (bestBoxIndex != null) {
+                boxes.get(bestBoxIndex).add(weight);
+            } else {
                 ArrayList<Integer> newBox = new ArrayList<>();
                 newBox.add(weight);
                 boxes.add(newBox);
